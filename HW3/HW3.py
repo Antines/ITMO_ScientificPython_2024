73711c08e6482cfb9091fc9ccbd3eb2ee91db66f
Raphael Dering <152785781+Antines@users.noreply.github.com>
2024-04-16 14:26:16 +0300

    Update HW3.py

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,52 +1,48 @@
 import openmeteo_requests
 from datetime import datetime
 
-class IncreaseSpeed():
-    def __init__(self, current_speed: int, max_speed: int):
-        self.current_speed = current_speed
-        self.max_speed = max_speed
+class IncreaseSpeed:
+    def __init__(self, car_instance):
+        self.car = car_instance
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current_speed >= self.max_speed:
+        if self.car.current_speed >= self.car.max_speed:
             raise StopIteration
         else:
-            self.current_speed += 10
-            return self.current_speed
+            self.car.current_speed = min(self.car.current_speed + 10, self.car.max_speed)
+            return self.car.current_speed
 
-class DecreaseSpeed():
-    def __init__(self, current_speed: int, max_speed: int):
-        self.current_speed = current_speed
-        self.max_speed = max_speed
-
+class DecreaseSpeed(IncreaseSpeed):
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current_speed <= 0:
+        if self.car.current_speed <= 0:
             raise StopIteration
         else:
-            self.current_speed -= 10
-            return self.current_speed
+            self.car.current_speed = max(self.car.current_speed - 10, 0)
+            return self.car.current_speed
 
-class Car():
+class Car:
     total_cars_on_road = 0
 
     def __init__(self, max_speed: int, current_speed=0, state="on road"):
         self.max_speed = max_speed
         self.current_speed = current_speed
         self.state = state
+        self.IncreaseSpeed = IncreaseSpeed(self)
+        self.DecreaseSpeed = DecreaseSpeed(self)
         Car.total_cars_on_road += 1
 
     def accelerate(self, upper_border=None):
         if self.state == "on road":
             if upper_border is not None and upper_border <= self.max_speed:
-                for speed in IncreaseSpeed(self.current_speed, self.max_speed):
+                for speed in self.IncreaseSpeed:
                     if speed > upper_border:
                         break
-                    self.current_speed = speed
                     print(f"Accelerating: Current speed is {self.current_speed} km/h")
             else:
                 self.current_speed = min(self.current_speed + 10, self.max_speed)
@@ -55,10 +51,9 @@ class Car():
     def brake(self, lower_border=None):
         if self.state == "on road":
             if lower_border is not None and lower_border <= self.max_speed:
-                for speed in DecreaseSpeed(self.current_speed):
+                for speed in self.DecreaseSpeed:
                     if speed < lower_border:
                         break
-                    self.current_speed = speed
                     print(f"Braking: Current speed is {self.current_speed} km/h")
             else:
                 self.current_speed = max(self.current_speed - 10, 0)
@@ -108,7 +103,7 @@ car1 = Car(90)
 car2 = Car(90)
 car3 = Car(90)
 car1.accelerate(60)
-car1.brake()
+car1.brake(30)
 car1.parking()
 print(Car.total_cars_on_road)
 Car.show_weather()
